0a9c39246d583c7356625678e789fa2fea5446a4
Diego Sano <diegocsano@outlook.com>
2023-09-14 13:17:46 -0300

    chore: install metaplex and config polyfills

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,15 @@
  */
 
 module.exports = {
+  resolver: {
+    extraNodeModules: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('readable-stream'),
+      zlib: require.resolve('browserify-zlib'),
+      path: require.resolve('path-browserify'),
+      url: require.resolve('react-native-url-polyfill'),
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
